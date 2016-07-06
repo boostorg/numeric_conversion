2009f0d4fc49a6db59b950a3140f8d1f58e1a8c6
Brandon Kohn <blkohn@hotmail.com>
2016-07-06 06:58:20 -0400

    Revert "removed deprecated std::unary_function from various conversion functors"
    This reverts commit 593aafab5a7aba86f14df5f9a14a53fc4353bb00.

--- include/boost/numeric/conversion/detail/converter.hpp
+++ include/boost/numeric/conversion/detail/converter.hpp
@@ -450,12 +450,12 @@ namespace boost { namespace numeric { namespace convdetail
   // Trivial Converter : used when (cv-unqualified) T == (cv-unqualified)  S
   //
   template<class Traits>
-  struct trivial_converter_impl : public dummy_range_checker<Traits>
+  struct trivial_converter_impl : public std::unary_function<  BOOST_DEDUCED_TYPENAME Traits::argument_type
+                                                              ,BOOST_DEDUCED_TYPENAME Traits::result_type
+                                                            >
+                                 ,public dummy_range_checker<Traits>
   {
     typedef Traits traits ;
-    
-    typedef typename Traits::argument_type argument_type;
-    typedef typename Traits::result_type result_type;
 
     typedef typename Traits::source_type   source_type   ;
     typedef typename Traits::argument_type argument_type ;
@@ -471,13 +471,13 @@ namespace boost { namespace numeric { namespace convdetail
   // Rounding Converter : used for float to integral conversions.
   //
   template<class Traits,class RangeChecker,class RawConverter,class Float2IntRounder>
-  struct rounding_converter : public RangeChecker
+  struct rounding_converter : public std::unary_function<  BOOST_DEDUCED_TYPENAME Traits::argument_type
+                                                          ,BOOST_DEDUCED_TYPENAME Traits::result_type
+                                                        >
+                             ,public RangeChecker
                              ,public Float2IntRounder
                              ,public RawConverter
   {
-    typedef typename Traits::argument_type argument_type;
-    typedef typename Traits::result_type result_type;
-    
     typedef RangeChecker     RangeCheckerBase ;
     typedef Float2IntRounder Float2IntRounderBase ;
     typedef RawConverter     RawConverterBase ;
@@ -501,12 +501,12 @@ namespace boost { namespace numeric { namespace convdetail
   // Non-Rounding Converter : used for all other conversions.
   //
   template<class Traits,class RangeChecker,class RawConverter>
-  struct non_rounding_converter : public RangeChecker
+  struct non_rounding_converter : public std::unary_function< BOOST_DEDUCED_TYPENAME Traits::argument_type
+                                                             ,BOOST_DEDUCED_TYPENAME Traits::result_type
+                                                           >
+                                 ,public RangeChecker
                                  ,public RawConverter
   {
-    typedef typename Traits::argument_type argument_type;
-    typedef typename Traits::result_type result_type;
-    
     typedef RangeChecker RangeCheckerBase ;
     typedef RawConverter RawConverterBase ;
 
